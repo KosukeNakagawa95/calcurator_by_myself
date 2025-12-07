b5e3512201431324313d38a69859ac676831fded
KosukeNakagawa95 <kna0891@gmail.com>
2025-12-07 17:44:04 +0900

    refactor: whileのなかのelseを削除 feat: エラーメッセージを優しい感じに修正 feat: メッセージをですます調に修正

--- calcurator_by_myself.py
+++ calcurator_by_myself.py
@@ -6,21 +6,20 @@ def get_valid_number(message):
             user_input = int(input(message))
             
             if user_input <= 0:
-                print("正しく入力せよ")
+                print("エラー：0以下の数字が入力されています")
                 continue
-
-            else:    
-                return  user_input 
+    
+            return  user_input 
         
         except ValueError:
-            print("半角数字を入力せよ")
+            print("エラー：半角数字を入力してください")
 
-quantity = get_valid_number("製作する個数を入力せよ")
-time_per_unit = get_valid_number("1個当たりの所要秒数を入力せよ")
+quantity = get_valid_number("製作する個数を入力してください")
+time_per_unit = get_valid_number("1個当たりの所要秒数を入力してください")
 
 total_minutes = math.ceil((quantity * time_per_unit) / 60)
 
-print(f"製作所要時間は{total_minutes}分")
+print(f"製作所要時間は{total_minutes}分です")
 
         
             
