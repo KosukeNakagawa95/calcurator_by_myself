4e6126f4e41d64dab03e213658560df5b8a1d47e
KosukeNakagawa95 <kna0891@gmail.com>
2025-12-07 17:18:11 +0900

    自力で書いてみた

---
+++ calcurator_by_myself.py
@@ -0,0 +1,20 @@
+import math
+
+def get_valid_number(message):
+    while True:
+        try:
+            user_input = int(input(message))
+            return user_input
+        
+        except ValueError:
+            print("半角数字を入力せよ")
+
+quantity = get_valid_number("製作する個数を入力せよ")
+time_per_unit = get_valid_number("1個当たりの所要秒数を入力せよ")
+
+total_minutes = math.ceil((quantity * time_per_unit) / 60)
+
+print(f"製作所要時間は{total_minutes}分")
+
+        
+            
